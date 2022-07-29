8f598cb452336bb63ef5a67512c991762b565e9d
Evan Hynes <evan.hynes@glimpseprotocol.io>
2022-07-29 15:04:26 +0100

    feat!: initialise app structure

---
+++ src/app/app.tsx
@@ -0,0 +1,5 @@
+import * as React from "react"
+
+export function App(): JSX.Element {
+  return <p>I am the app</p>
+}
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+import * as ReactDom from "react-dom"
+
+import { App } from "@src/app"
+
+
+ReactDom.render(<App />, document.getElementById("root"))
+
+if (module.hot) {
+  module.hot.accept()
+}
