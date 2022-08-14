4ff071c162888c3edf9e338c77f7bea9d184b73a
Evan Hynes <evan.hynes@glimpseprotocol.io>
2022-08-14 18:28:40 +0100

    feat: add basic styling to record cards

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,14 +2,22 @@ import * as React from "react"
 
 import { Box } from "@mui/system"
 
+import { useMusicResultContext } from "@src/providers"
+
+import { RecordCard } from "./record-card"
 import { SearchBar } from "./search-bar"
-import { SearchResults } from "./search-results"
 
 export function Home(): JSX.Element {
+  const { musicResult } = useMusicResultContext()
+
   return (
     <Box>
       <SearchBar />
-      <SearchResults />
+      <Box sx={{ maxWidth: "1920px", m: "auto" }}>
+        {musicResult.map((result, index) => {
+          return <RecordCard key={index} {...result}  />
+        })}
+      </Box>
     </Box>
   )
 }
